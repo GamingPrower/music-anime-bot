1637bcd012b25c1227727ec43c3088a37c1cb4f3
GamingPrower <dcody12@hotmail.com>
2019-01-11 01:38:07 -0500

    refactored search for better performance

--- commands/mal/search.js
+++ commands/mal/search.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Promise = require('es6-promise').Promise;
 const Commando = require('discord.js-commando');
 const malScraper = require('mal-scraper');
 const Discord = require('discord.js');
@@ -15,28 +16,45 @@ class MALCommand extends Commando.Command {
 		});
 	}
 
-	async run(message, args) {
-		const resultsMal = await malScraper.getInfoFromName(args);
-		const resultsAni = await masterani.getSearch(args);
-		const attachment = new Discord.Attachment(path.join(__dirname, '/masterani.jpg'), 'masterani.jpg');
+	run(message, args) {
+		const malSearch = () => {
+			const promise = new Promise(resolve => {
+				resolve(malScraper.getInfoFromName(args));
+			});
+			return promise;
+		};
 
-		const malEmbed = new Discord.RichEmbed()
-			.setTitle(resultsMal.title)
-			.addField('Synopsis', `${resultsMal.synopsis.substr(0, 1020)}...`)
-			.setColor(0x000000)
-			.setThumbnail(resultsMal.picture)
-			.setURL(resultsMal.url)
-			.setFooter(`Score: ${resultsMal.score}`);
-		message.channel.send(malEmbed);
+		const aniSearch = () => {
+			const promise = new Promise(resolve => {
+				resolve(masterani.getSearch(args));
+			});
+			return promise;
+		};
 
-		const aniEmbed = new Discord.RichEmbed()
-			.setTitle(`Watch "${resultsAni[0].title} " on Masterani.me`)
-			.setDescription('masterani.me - is an anime info database with a streaming option to watch anime in HD.')
-			.setURL(resultsAni[0].episodesListHref)
-			.setColor(0x000000)
-			.attachFile(attachment)
-			.setThumbnail('attachment://masterani.jpg');
-		message.channel.send(aniEmbed);
+		malSearch()
+			.then(mal => {
+				const malEmbed = new Discord.RichEmbed()
+					.setTitle(mal.title)
+					.addField('Synopsis', `${mal.synopsis.substr(0, 1020)}...`)
+					.setColor(0x000000)
+					.setThumbnail(mal.picture)
+					.setURL(mal.url)
+					.setFooter(`Score: ${mal.score}`);
+				message.channel.send(malEmbed);
+				return aniSearch();
+			})
+			.then(ani => {
+				const attachment = new Discord.Attachment(path.join(__dirname, '/masterani.jpg'), 'masterani.jpg');
+				const aniEmbed = new Discord.RichEmbed()
+					.setTitle(`Watch "${ani[0].title} " on Masterani.me`)
+					.setDescription('masterani.me - is an anime info database with a streaming option to watch anime in HD.')
+					.setURL(ani[0].episodesListHref)
+					.setColor(0x000000)
+					.attachFile(attachment)
+					.setThumbnail('attachment://masterani.jpg');
+				message.channel.send(aniEmbed);
+			})
+			.catch(err => console.error(err));
 	}
 }
 
